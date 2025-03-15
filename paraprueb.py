28a20014e4bfc0b34db06d50dce239dcb3e680e7
carlos <vasquezjalpacarlos@hotmail.com>
2025-03-14 22:36:33 -0600

    actualizacion viernes

--- paraprueb.py
+++ paraprueb.py
@@ -1,4 +1,6 @@
 from CNN1_inf import CNN1_inf as CNN1
-case = CNN1('./Data CNN1/No target/10.jpg')
+from CNN2_inf import CNN2_inf as CNN2
 
-print(case)
+case = CNN1('./Data CNN1/No target/10.jpg')
+case2 = CNN2('./Data CNN2/010/10.jpg')
+print(case, case2)
