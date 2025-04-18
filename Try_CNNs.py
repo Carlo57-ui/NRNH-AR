3c3a8e80f78e79754c34da8458b32bcd95872aec
carlos <vasquezjalpacarlos@hotmail.com>
2025-04-18 11:43:35 -0600

    Actualization TryCNN

--- Try_CNNs.py
+++ Try_CNNs.py
@@ -1,6 +1,8 @@
 from CNN1_inf import CNN1_inf as CNN1
 from CNN2_inf import CNN2_inf as CNN2
 
+'''With data pictures'''
+
 case = CNN1('./Data CNN1/No target/10.jpg')
 case = case.predicted_class
 
@@ -9,3 +11,16 @@ case2 = case2.predicted_class
 
 print("CASE CNN1", case)
 print("CASE CNN2", case2)
+
+
+'''With robot'''
+import cv2
+cam = cv2.VideoCapture(0)
+ret,frame = cam.read()
+cv2.imwrite("TryCNN.jpg",frame) 
+cam.release()
+
+case = CNN1('TryCNN.jpg')
+case = case.predicted_class
+
+print("CASE CNN1", case)
